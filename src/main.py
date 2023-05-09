039d31ada8cfc8464d4a1524f95a96853e1e9b94
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-05-09 21:15:45 +0200

    Add mock main-function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
+from compiler import compile
+from interpreter import Interpreter
+from parser import Parser
+from tokenizer import Tokenizer
+
 def main():
-    ...
+    text = "f(t,v,s)=-1/2*g*t**2+v*t+s h(t)=f(t,0,0) g=9.81 max(h(1), 0) f(1, 5, 5) f max"
+    tokenizer = Tokenizer(text)
+    parser = Parser(tokenizer)
+    interpreter = Interpreter()
+    for statement in parser:
+        instructions = compile(statement)
+        for i in instructions:
+            interpreter.interpret(i)
+    print("Stack:")
+    interpreter.print_stack()
+    print("Namespace:")
+    interpreter.print_namespace()
 
 if __name__ == "__main__":
     main()
